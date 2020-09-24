8d024769e85842b928efb00150dd68d0ebb5eaa2
John Day <john@deepfactor.io>
2020-09-24 18:30:28 +0000

    Corrected db naming and config

--- src/capplication/docker/main.c
+++ src/capplication/docker/main.c
@@ -11,24 +11,27 @@
 
 #define PORT 8080
 
-void do_exit(PGconn *conn) {
-    
-    PQfinish(conn);
-    exit(1);
+void do_exit(PGconn *conn)
+{
+
+  PQfinish(conn);
+  exit(1);
 }
 
 /**
  * Callback function for the web application on /getstocks url call
  */
-int callback_getstocks (const struct _u_request * request, struct _u_response * response, void * user_data) {
+int callback_getstocks(const struct _u_request *request, struct _u_response *response, void *user_data)
+{
   printf("in getstocks");
-  PGconn *conn = PQconnectdb("host=postgresql-demo user=postgres password=gRAHBwPjR8 dbname=stocksdb");
+  PGconn *conn = PQconnectdb("host=postgresql user=postgres password=gRAHBwPjR8 dbname=stocksdb");
 
-  if (PQstatus(conn) == CONNECTION_BAD) {
+  if (PQstatus(conn) == CONNECTION_BAD)
+  {
 
-      fprintf(stderr, "Connection to database failed: %s\n",
+    fprintf(stderr, "Connection to database failed: %s\n",
             PQerrorMessage(conn));
-        do_exit(conn);
+    do_exit(conn);
   }
 
   char *user = PQuser(conn);
@@ -41,11 +44,12 @@ int callback_getstocks (const struct _u_request * request, struct _u_response * 
 
   PGresult *res = PQexec(conn, "SELECT ticker FROM stocks");
 
-  if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+  if (PQresultStatus(res) != PGRES_TUPLES_OK)
+  {
 
-      printf("No data retrieved\n");
-      PQclear(res);
-      do_exit(conn);
+    printf("No data retrieved\n");
+    PQclear(res);
+    do_exit(conn);
   }
 
   printf("%s\n", PQgetvalue(res, 0, 0));
@@ -57,29 +61,33 @@ int callback_getstocks (const struct _u_request * request, struct _u_response * 
   return U_CALLBACK_CONTINUE;
 }
 
-
 /**
  * main function
  */
-int main(void) {
+int main(void)
+{
   struct _u_instance instance;
 
   // Initialize instance with the port number
-  if (ulfius_init_instance(&instance, PORT, NULL, NULL) != U_OK) {
+  if (ulfius_init_instance(&instance, PORT, NULL, NULL) != U_OK)
+  {
     fprintf(stderr, "Error ulfius_init_instance, abort\n");
-    return(1);
+    return (1);
   }
 
   // Endpoint list declaration
   ulfius_add_endpoint_by_val(&instance, "GET", "/getstocks", NULL, 0, &callback_getstocks, NULL);
 
   // Start the framework
-  if (ulfius_start_framework(&instance) == U_OK) {
+  if (ulfius_start_framework(&instance) == U_OK)
+  {
     printf("Start framework on port %d\n", instance.port);
 
-    while(1)
+    while (1)
       getchar();
-  } else {
+  }
+  else
+  {
     fprintf(stderr, "Error starting framework\n");
   }
   printf("End framework\n");
